3f012d425d745163639b42227f23cde8e86966f2
Unknown <haar@uni-bremen.de>
2017-12-12 09:39:18 +0100

    Unnötigen Import entfernt

--- knowledge/knowledge/prolog/init.pl
+++ knowledge/knowledge/prolog/init.pl
@@ -1,5 +1,4 @@
 :- register_ros_package(poke_position_calculation).
 :- register_ros_package(kitchen_model_export).
-:- register_ros_package(knowrob_map_tools).
 
 :- owl_parser:owl_parse('package://iai_semantic_maps/owl/room.owl').
