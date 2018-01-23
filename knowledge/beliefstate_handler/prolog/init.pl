dbaa1213ab98e7d6e6d40f89ff8f06a288183057
Unknown <haar@uni-bremen.de>
2018-01-23 10:47:55 +0100

    Beliefestate erstmal entfernt

--- knowledge/beliefstate_handler/prolog/init.pl
+++
@@ -1,4 +0,0 @@
-:- register_ros_package(knowrob_beliefstate).
-
-:- owl_parser:owl_parse('package://poke_position_calculation/owl/suturo_object.owl').
-:- rdf_register_prefix(suturo_object, 'http://knowrob.org/kb/suturo_object.owl#').
