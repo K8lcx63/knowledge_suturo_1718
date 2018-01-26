754adfacd08859509acf97cdf09aaa9af41a09b9
Unknown <haar@uni-bremen.de>
2018-01-26 11:54:33 +0100

    suturo_object.owl erweitert und in knowledge verschoben

--- knowledge/poke_position_calculation/prolog/init.pl
+++ knowledge/poke_position_calculation/prolog/init.pl
@@ -2,4 +2,4 @@
 
 :- use_module(poke_position_calculator,[calculate_poke_position_left/6, calculate_poke_position_right/6]).
 
-:- owl_parser:owl_parse('../owl/suturo_object.owl').
+:- owl_parser:owl_parse('package://knowledge/owl/suturo_object.owl').
