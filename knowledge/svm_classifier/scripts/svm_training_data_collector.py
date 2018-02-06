47f049b137979173e724ffb06a5668d5fef4e4b2
Unknown <haar@uni-bremen.de>
2018-02-06 09:15:07 +0100

    training daten sammeln auf csv umgestellt

--- knowledge/svm_classifier/scripts/svm_training_data_collector.py
+++ knowledge/svm_classifier/scripts/svm_training_data_collector.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python
 
 import rospy
+import numpy as np
 import rospkg
 import pickle
-
-from knowledge_msgs.msg import TrainingData
+import csv
+import os
 
 def save_training_set():
-    rospack = rospkg.RosPack()
-    packagePath = rospack.get_path('svm_classifier')
     fullPath = packagePath + '/data/training_set.sav'
     pickle.dump(labeled_features, open(fullPath, 'wb'))
 
     print('Save training set to disk')
 
-def collect_training_data(training_data_msg):
-    labeled_features.append([training_data_msg.features, training_data_msg.label])
-    print('Received message with label: ' + training_data_msg.label)
+def get_features(label, color_histogram_file, normals_histogram_file):
+    print('process ' + color_histogram_file + ' and ' + normals_histogram_file)
+
+    color_histogram_path = packagePath + '/data/histograms/color_histograms/' + label + '/' + color_histogram_file
+    normals_histogram_path = packagePath + '/data/histograms/normals_histograms/' + label + '/' + normals_histogram_file
+
+    color_histogram_reader = csv.reader(open(color_histogram_path))
+    color_histogram = np.array(map(int, next(color_histogram_reader)))
+
+    normals_histogram_reader = csv.reader(open(normals_histogram_path))
+    normals_histogram = np.array(map(int, next(normals_histogram_reader)))
+
+    return np.concatenate((color_histogram, normals_histogram))
+
+def collect_training_data():
+    labels = ['tomato_sauce_oro_di_parma', 'hela_curry_ketchup', 
+              'pringles_paprika', 'pringles_salt', 
+              'ja_milch', 'koelln_muesli_knusper_honig_nuss', 'kelloggs_toppas_mini',
+              'cup_eco_orange', 'edeka_red_bowl', 'sigg_bottle']
+
+    for label in labels:
+        color_histogram_files = os.listdir(packagePath + '/data/histograms/color_histograms/' + label)
+        normals_histogram_files = os.listdir(packagePath + '/data/histograms/normals_histograms/' + label)
+        for i in range(len(color_histogram_files)):
+            features = get_features(label, color_histogram_files[i], normals_histogram_files[i])
+            labeled_features.append([features, label])
 
 if __name__ == '__main__':
-    rospy.init_node('svm_classifier')
-    rospy.on_shutdown(save_training_set)
+    rospy.init_node('svm_training_data_collector')
+
+    rospack = rospkg.RosPack()
+    packagePath = rospack.get_path('svm_classifier')
 
     labeled_features = []
-    subscriber = rospy.Subscriber('/feature_extractor/cloud_features' , TrainingData, collect_training_data)
-    print('Collecting data...')
-    
-    rospy.spin()
+    collect_training_data()
+    save_training_set()
