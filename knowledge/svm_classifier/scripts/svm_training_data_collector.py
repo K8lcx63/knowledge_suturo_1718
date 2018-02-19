6122dea70c21a8e4ecdb3093c63629757ff6fdfa
Unknown <haar@uni-bremen.de>
2018-02-19 16:28:22 +0100

    neue Frabhistogramme

--- knowledge/svm_classifier/scripts/svm_training_data_collector.py
+++ knowledge/svm_classifier/scripts/svm_training_data_collector.py
@@ -13,14 +13,22 @@ def save_training_set():
 
     print('Save training set to disk')
 
-def get_features(label, color_histogram_file, normals_histogram_file):
+def get_corresponding_normals_histogram_file(color_histogram_file):
+    temp = color_histogram_file.split('_')
+    file_prefix = temp[0] + '_' + temp[1] + '_' + temp[2]
+    normals_histogram_file = file_prefix + '_normals_histogram.csv'
+
+    return normals_histogram_file
+
+def get_features(label, color_histogram_file):
+    normals_histogram_file = get_corresponding_normals_histogram_file(color_histogram_file)
     print('process ' + color_histogram_file + ' and ' + normals_histogram_file)
 
     color_histogram_path = packagePath + '/data/histograms/color_histograms/' + label + '/' + color_histogram_file
     normals_histogram_path = packagePath + '/data/histograms/normals_histograms/' + label + '/' + normals_histogram_file
 
     color_histogram_reader = csv.reader(open(color_histogram_path))
-    color_histogram = np.array(map(int, next(color_histogram_reader)))
+    color_histogram = np.array(map(int, next(color_histogram_reader)[:-1]))
 
     normals_histogram_reader = csv.reader(open(normals_histogram_path))
     normals_histogram = np.array(map(int, next(normals_histogram_reader)))
@@ -35,9 +43,8 @@ def collect_training_data():
 
     for label in labels:
         color_histogram_files = os.listdir(packagePath + '/data/histograms/color_histograms/' + label)
-        normals_histogram_files = os.listdir(packagePath + '/data/histograms/normals_histograms/' + label)
         for i in range(len(color_histogram_files)):
-            features = get_features(label, color_histogram_files[i], normals_histogram_files[i])
+            features = get_features(label, color_histogram_files[i])
             labeled_features.append([features, label])
 
 if __name__ == '__main__':
