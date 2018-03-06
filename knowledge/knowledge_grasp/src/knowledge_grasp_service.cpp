c6ee2bfcaee80829f948a964e7c4e13797db24a5
Unknown <maxbahr@uni-bremen.de>
2018-03-06 13:04:24 +0100

    knowledge grasp absturz gefixed + in launchfiles eingetragen

--- knowledge/knowledge_grasp/src/knowledge_grasp_service.cpp
+++ knowledge/knowledge_grasp/src/knowledge_grasp_service.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <sstream>
+#include <locale>
 #include <ros/ros.h>
 #include <knowledge_msgs/GraspIndividual.h>
 #include <knowledge_common/prolog_util.h>
@@ -17,27 +18,37 @@ std::string createQuery(std::string object_label)
   return ss.str();
 }
 
+std::string translateToUnderscore(std::string label){
+
+  std::stringstream rs;
+
+  rs << std::tolower(label.at(0), std::locale());
+
+  for(unsigned i=1; i<label.length(); ++i){
+    if(std::isupper(label.at(i))){
+      rs << "_" << std::tolower(label.at(i), std::locale());
+    } else {
+      rs << label.at(i);
+    }
+  }
+  return rs.str();
+
+}
+
 bool find_grasp_pose(knowledge_msgs::GraspIndividual::Request &req, knowledge_msgs::GraspIndividual::Response &res)
 {
     Prolog pl;
-    PrologBindings bdg = pl.once(createQuery(req.object_label));
-
-    if(&bdg != NULL)
+    PrologQueryProxy bdgs = pl.query(createQuery(req.object_label));
+ 
+  for(PrologQueryProxy::iterator it=bdgs.begin();
+      it != bdgs.end(); it++)
     {
-      if(req.object_label == "JaMilch"){res.grasp_pose.header.frame_id = "/ja_milch";}
-      if(req.object_label == "HelaCurryKetchup"){res.grasp_pose.header.frame_id = "/hela_curry_ketchup";}
-      if(req.object_label == "SiggBottle"){res.grasp_pose.header.frame_id = "/sigg_bottle";}
-      if(req.object_label == "KeloggsToppasMini"){res.grasp_pose.header.frame_id = "/keloggs_toppas_mini";}
-      if(req.object_label == "CupEcoOrange"){res.grasp_pose.header.frame_id = "/cup_eco_orange";}
-      if(req.object_label == "EdekaRedBowl"){res.grasp_pose.header.frame_id = "/edeka_red_bowl";}
-      if(req.object_label == "PringlesSalt"){res.grasp_pose.header.frame_id = "/pringles_salt";}
-      if(req.object_label == "PringlesPaprika"){res.grasp_pose.header.frame_id = "/pringles_paprika";}
-      if(req.object_label == "KoellnMuesliKnusperHonigNuss"){res.grasp_pose.header.frame_id = "/koelln_muesli_knusper_honig_nuss";}
-      if(req.object_label == "TomatoSauceOroDiParma"){res.grasp_pose.header.frame_id = "/tomato_sauce_oro_di_parma";}
-      //res.grasp_pose.header.frame_id = "/" + req.object_label;
+      PrologBindings bdg = *it;
+      res.grasp_pose.header.frame_id = "/" + translateToUnderscore(req.object_label);
 	    res.grasp_pose.pose =  PrologUtil::prologBindingToPose(bdg, "Position", "Quaternion");
       return true;
-	  } 
+	  }
+    ROS_ERROR_STREAM("LABEL \"" << req.object_label << "\"NICHT ERKANNT! BITTE EINGABEN PRUEFEN!");
     return false; 
 }
    
