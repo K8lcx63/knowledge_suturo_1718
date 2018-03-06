53c432ead640121dfc4d1e182540e7b9201cbf45
Unknown <maxbahr@uni-bremen.de>
2018-03-06 01:38:46 +0100

    graspposen neu eingelesen und header frame aus label übersetzt

--- knowledge/knowledge_grasp/src/knowledge_grasp_service.cpp
+++ knowledge/knowledge_grasp/src/knowledge_grasp_service.cpp
@@ -24,7 +24,17 @@ bool find_grasp_pose(knowledge_msgs::GraspIndividual::Request &req, knowledge_ms
 
     if(&bdg != NULL)
     {
-      res.grasp_pose.header.frame_id = "/" + req.object_label;
+      if(req.object_label == "JaMilch"){res.grasp_pose.header.frame_id = "/ja_milch";}
+      if(req.object_label == "HelaCurryKetchup"){res.grasp_pose.header.frame_id = "/hela_curry_ketchup";}
+      if(req.object_label == "SiggBottle"){res.grasp_pose.header.frame_id = "/sigg_bottle";}
+      if(req.object_label == "KeloggsToppasMini"){res.grasp_pose.header.frame_id = "/keloggs_toppas_mini";}
+      if(req.object_label == "CupEcoOrange"){res.grasp_pose.header.frame_id = "/cup_eco_orange";}
+      if(req.object_label == "EdekaRedBowl"){res.grasp_pose.header.frame_id = "/edeka_red_bowl";}
+      if(req.object_label == "PringlesSalt"){res.grasp_pose.header.frame_id = "/pringles_salt";}
+      if(req.object_label == "PringlesPaprika"){res.grasp_pose.header.frame_id = "/pringles_paprika";}
+      if(req.object_label == "KoellnMuesliKnusperHonigNuss"){res.grasp_pose.header.frame_id = "/koelln_muesli_knusper_honig_nuss";}
+      if(req.object_label == "TomatoSauceOroDiParma"){res.grasp_pose.header.frame_id = "/tomato_sauce_oro_di_parma";}
+      //res.grasp_pose.header.frame_id = "/" + req.object_label;
 	    res.grasp_pose.pose =  PrologUtil::prologBindingToPose(bdg, "Position", "Quaternion");
       return true;
 	  } 
