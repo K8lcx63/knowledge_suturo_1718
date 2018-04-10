7ea7a7ca42ff4e6d60b4692a5a9745912ffcec42
Unknown <haar@uni-bremen.de>
2018-04-10 15:10:34 +0200

    beliefstate failurehandling + meshes umbenannt

--- knowledge/beliefstate/scripts/beliefstate.py
+++ knowledge/beliefstate/scripts/beliefstate.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python
 
 import rospy
-import roslib; roslib.load_manifest('json_prolog')
-
-import rospy
 from json_prolog import json_prolog
 from knowledge_msgs.msg import *
 from knowledge_msgs.srv import * 
 from geometry_msgs.msg import *
-import re
 import tf
 from std_msgs.msg import *
 from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback, Marker
 
-
-def to_underscore(name):
-    s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
-    return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
+def is_known_object_label(label):
+    labels =  ['TomatoSauceOroDiParma', 'HelaCurryKetchup', 
+              'PringlesPaprika', 'PringlesSalt', 
+              'JaMilch', 'KoellnMuesliKnusperHonigNuss', 'KellogsToppasMini',
+              'CupEcoOrange', 'EdekaRedBowl', 'SiggBottle']
+    return label in labels
 
 def object_url_to_object_name(url):
   splitted_url = url.split("#")
@@ -57,44 +55,98 @@ def create_query_for_drop_object(drop_object_msg):
 def create_query_object_attached_to_gripper(gripper):
   return "object_attached_to_gripper(suturo_action:\'" + gripper_as_string(gripper) + "\',ObjectIndividual)"
 
-def object_exists(object_class):
-  query_result = prolog.once("object_exists(suturo_object:\'" + object_class + "\')")
-  return prolog_query_true(query_result)
+def object_exists(object_label):
+    if not is_known_object_label(object_label):
+        rospy.logerr("Label: \'" + object_label + "\' is an unknonw label!")
+        return False
+
+    try:
+        query_result = prolog.once("object_exists(suturo_object:\'" + object_label + "\')")
+    except:
+        return False
+    else:
+        return prolog_query_true(query_result)
 
 def process_perceive_action(perceive_object_msg):
-    #perceive_object_msg.object_pose.pose.orientation.x = 0.0
-    #perceive_object_msg.object_pose.pose.orientation.y = 0.0
-    #perceive_object_msg.object_pose.pose.orientation.z = 0.0
-    #perceive_object_msg.object_pose.pose.orientation.w = 1.0
+    if not is_known_object_label(perceive_object_msg.object_label):
+        rospy.logerr("Label: \'" + perceive_object_msg.object_label + "\' is an unknonw label!")
+        return
+
     exists = object_exists(perceive_object_msg.object_label)
-    prolog.once(create_query_for_perceive_object(perceive_object_msg))
-    if not exists:
-        spawn_object_frame(perceive_object_msg.object_label, perceive_object_msg.object_pose)
-        publish_collision_object(perceive_object_msg)
+    try:
+        prolog.once(create_query_for_perceive_object(perceive_object_msg))
+    except:
+        rospy.logerr("Perceiveaction failed, because the prolog query failed!")
     else:
-        update_object_frame(perceive_object_msg.object_label, perceive_object_msg.object_pose)
+        if not exists:
+            spawn_object_frame(perceive_object_msg.object_label, perceive_object_msg.object_pose)
+            publish_collision_object(perceive_object_msg)
+        else:
+            update_object_frame(perceive_object_msg.object_label, perceive_object_msg.object_pose)
 
 def process_grasp_action(grasp_object_msg):
-  prolog.once(create_query_for_grasp_object(grasp_object_msg))
-  if(grasp_object_msg.gripper.gripper == Gripper.LEFT_GRIPPER):
-    change_reference_frame(grasp_object_msg.object_label, "l_gripper_led_frame") 
-  else:
-    change_reference_frame(grasp_object_msg.object_label, "r_gripper_led_frame")
+    #check if its a known label
+    if not is_known_object_label(grasp_object_msg.object_label):
+        rospy.logerr("Label: \'" + grasp_object_msg.object_label + "\' is an unknonw label!")
+        return
+
+    #check if the object already exists
+    exists = object_exists(grasp_object_msg.object_label)
+    if not exists:
+        rospy.logerr("Graspaction failed, because object " + grasp_object_msg.object_label + " is never perceived!")
+        return
+
+    try:
+        prolog.once(create_query_for_grasp_object(grasp_object_msg))
+    except:
+        rospy.logerr("Graspaction failed, because the prolog query failed!")
+    else:
+        if(grasp_object_msg.gripper.gripper == Gripper.LEFT_GRIPPER):
+            change_reference_frame(grasp_object_msg.object_label, "l_gripper_led_frame") 
+        else:
+            change_reference_frame(grasp_object_msg.object_label, "r_gripper_led_frame")
 
 def process_drop_action(drop_object_msg):
-  query_result = prolog.once(create_query_object_attached_to_gripper(drop_object_msg.gripper.gripper))
-  prolog.once(create_query_for_drop_object(drop_object_msg))
-  object_name = object_url_to_object_name(query_result["ObjectIndividual"])
-  change_reference_frame(object_name, "map")
+    try:
+        query_result = prolog.once(create_query_object_attached_to_gripper(drop_object_msg.gripper.gripper))
+    except:
+        rospy.logerr("Dropaction failed, because the prolog query failed!")
+    else:
+        if prolog_query_true(query_result):
+            try:
+                prolog.once(create_query_for_drop_object(drop_object_msg))
+            except:
+                rospy.logerr("Dropaction failed, because the prolog query failed!")
+            else:
+                object_name = object_url_to_object_name(query_result["ObjectIndividual"])
+                change_reference_frame(object_name, "map")
+        else:
+            rospy.logerr("Dropaction failed, because no object is attached to the gripper!")
   
 def gripper_empty(req):
-  left_gripper_query_result = prolog.once(create_query_object_attached_to_gripper(Gripper.LEFT_GRIPPER))
-  right_gripper_query_result = prolog.once(create_query_object_attached_to_gripper(Gripper.RIGHT_GRIPPER))
+    try:
+        left_gripper_query_result = prolog.once(create_query_object_attached_to_gripper(Gripper.LEFT_GRIPPER))
+        right_gripper_query_result = prolog.once(create_query_object_attached_to_gripper(Gripper.RIGHT_GRIPPER))
+    except:
+        rospy.logerr("EmptyGripperService failed, because the prolog query failed!")
+    else:
+        left_gripper_empty = prolog_query_false(left_gripper_query_result)
+        right_gripper_empty = prolog_query_false(right_gripper_query_result)
+        return EmptyGripperResponse(left_gripper_empty, right_gripper_empty)
 
-  left_gripper_empty = prolog_query_false(left_gripper_query_result)
-  right_gripper_empty = prolog_query_false(right_gripper_query_result)
+    return EmptyGripperResponse(False, False)
 
-  return EmptyGripperResponse(left_gripper_empty, right_gripper_empty)
+def object_attached_to_gripper(req):
+    try:
+        query_result = prolog.once(create_query_object_attached_to_gripper(req.gripper.gripper))
+    except:
+        rospy.logerr("ObjectAttachedToGripperService failed, because the prolog query failed!")
+    else:
+        if(prolog_query_true(query_result)):
+            object_name = object_url_to_object_name(query_result["ObjectIndividual"])
+            return GetObjectAttachedToGripperResponse(object_name)
+
+    return GetObjectAttachedToGripperResponse("")
 
 def objects_to_pick(req):
     query_result = prolog.once("get_two_objects_on_kitchen_island_counter_with_same_storage_place(Object1, Object2)")
@@ -120,8 +172,7 @@ def pose_stamped_to_position_tupel(pose_stamped):
 def pose_stamped_to_quaternion_tupel(pose_stamped):
     return (pose_stamped.pose.orientation.x, pose_stamped.pose.orientation.y, pose_stamped.pose.orientation.z, pose_stamped.pose.orientation.w)
 
-def spawn_object_frame(object_name, object_pose):
-    object_frame = to_underscore(object_name)
+def spawn_object_frame(object_frame, object_pose):
     source_frame_id = object_pose.header.frame_id
     if source_frame_id.startswith('/'):
         source_frame_id = source_frame_id[1:]
@@ -136,10 +187,9 @@ def spawn_object_frame(object_name, object_pose):
     map_pose.header.frame_id = "map"
     object_frames[object_frame] = map_pose
 
-    spawn_object_mesh(object_name)
+    spawn_object_mesh(object_frame)
 
-def update_object_frame(object_name, object_pose):
-    object_frame = to_underscore(object_name)
+def update_object_frame(object_frame, object_pose):
     source_frame_id = object_pose.header.frame_id
     if source_frame_id.startswith('/'):
         source_frame_id = source_frame_id[1:]
@@ -154,21 +204,18 @@ def update_object_frame(object_name, object_pose):
     map_pose.header.frame_id = "map"
     object_frames[object_frame] = map_pose
 
-def change_reference_frame(object_name, new_reference_frame_id):
-    object_frame = to_underscore(object_name)
+def change_reference_frame(object_frame, new_reference_frame_id):
     old_frame_pose = object_frames[object_frame]
 
     new_frame_pose = transform_listener.transformPose(new_reference_frame_id, old_frame_pose)
     object_frames[object_frame] = new_frame_pose
 
-def spawn_object_mesh(object_name):
-    object_frame = to_underscore(object_name)
-
+def spawn_object_mesh(object_frame):
     mesh_marker = Marker()
     mesh_marker.header.frame_id = object_frame
     mesh_marker.header.stamp = rospy.get_rostime()
     mesh_marker.pose.orientation.w = 1
-    mesh_marker.ns = object_name
+    mesh_marker.ns = object_frame
     mesh_marker.id = 1
     mesh_marker.action = Marker.ADD
     mesh_marker.type = Marker.MESH_RESOURCE
@@ -181,14 +228,17 @@ def spawn_object_mesh(object_name):
     object_marker.append(mesh_marker)
 
 def publish_collision_object(perceive_object_msg):
-  query_result = prolog.once("mesh_path(suturo_object:\'" + perceive_object_msg.object_label + "\', MeshPath)")
-  mesh_path = query_result["MeshPath"]
-
-  collision_object_msg = PerceivedObjectBoundingBox()
-  collision_object_msg.object_label = perceive_object_msg.object_label
-  collision_object_msg.mesh_path = mesh_path[1:-1]
-  collision_object_msg.pose = perceive_object_msg.object_pose
-  collision_object_publisher.publish(collision_object_msg)
+    try:
+        query_result = prolog.once("mesh_path(suturo_object:\'" + perceive_object_msg.object_label + "\', MeshPath)")
+        mesh_path = query_result["MeshPath"]
+    except:
+        rospy.logerr("CollisionObjectPublisher failed, because the prolog query failed!")
+    else:
+        collision_object_msg = PerceivedObjectBoundingBox()
+        collision_object_msg.object_label = perceive_object_msg.object_label
+        collision_object_msg.mesh_path = mesh_path[1:-1]
+        collision_object_msg.pose = perceive_object_msg.object_pose
+        collision_object_publisher.publish(collision_object_msg)
 
 def object_frame_broadcast(event):
     for frame_id, frame_pose in object_frames.iteritems():
@@ -198,14 +248,6 @@ def object_mesh_broadcast(event):
     for marker in object_marker:
       marker.header.stamp = rospy.get_rostime()
       marker_pub.publish(marker)
-
-def object_attached_to_gripper(req):
-    query_result = prolog.once(create_query_object_attached_to_gripper(req.gripper.gripper))
-    if(prolog_query_true(query_result)):
-        object_name = object_url_to_object_name(query_result["ObjectIndividual"])
-        return GetObjectAttachedToGripperResponse(object_name)
-    else:
-        return GetObjectAttachedToGripperResponse("")
 
 def clear_beliefstate(msg):
     prolog.once("clear")
@@ -235,14 +277,3 @@ if __name__ == '__main__':
     rospy.Timer(rospy.Duration(0.02), object_frame_broadcast)
     rospy.Timer(rospy.Duration(0.02), object_mesh_broadcast)
     rospy.spin()
-
-    # rate = rospy.Rate(50.0)
-    # while not rospy.is_shutdown():
-    #     for frame_id, frame_pose in object_frames.iteritems():
-    #         transform_broadcaster.sendTransform(pose_stamped_to_position_tupel(frame_pose), pose_stamped_to_quaternion_tupel(frame_pose), rospy.Time.now(), frame_id, frame_pose.header.frame_id)
-            
-    #     for marker in object_marker:
-    #         marker.header.stamp = rospy.get_rostime()
-    #         marker_pub.publish(marker)
-
-    #     rate.sleep()
