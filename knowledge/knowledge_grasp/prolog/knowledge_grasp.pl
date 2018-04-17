50603294eb3c530fcebd898def7e13451748e6de
Unknown <maxbahr@uni-bremen.de>
2018-04-17 15:56:28 +0200

    Knowledge Grasp liefert jetzt Liste und Richtungen

--- knowledge/knowledge_grasp/prolog/knowledge_grasp.pl
+++ knowledge/knowledge_grasp/prolog/knowledge_grasp.pl
@@ -1,6 +1,6 @@
 :- module(knowledge_grasp,
     [
-      find_grasp_pose/3
+      find_grasp_pose/4
     ]).
 
 :- use_module(library('semweb/rdfs')).
@@ -14,9 +14,11 @@
 :- rdf_register_prefix(suturo_object, 'http://knowrob.org/kb/suturo_object.owl#').
 
 :-  rdf_meta
-    find_grasp_pose(r,-, -).
+    find_grasp_pose(r,-,-,-).
 
-find_grasp_pose(ObjectClassLabel, Translation, Quaternion):-
+find_grasp_pose(ObjectClassLabel, Translation, Quaternion, Direction):-
 	 owl_class_properties_value(ObjectClassLabel, suturo_object:'graspableAt', GraspPose),
+	 rdf(GraspPose, suturo_object:'graspDirection', LitDirection),
+	 strip_literal_type(LitDirection, Direction),
 	 transform_data(GraspPose, (Translation, Quaternion)).
-
+	 
