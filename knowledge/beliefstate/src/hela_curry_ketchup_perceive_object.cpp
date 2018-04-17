52ff972893abf623c7c22428ad8582a35838ba4d
Unknown <rhaak@uni-bremen.de>
2018-04-17 14:21:05 +0200

    ketchup besser position

--- knowledge/beliefstate/src/hela_curry_ketchup_perceive_object.cpp
+++ knowledge/beliefstate/src/hela_curry_ketchup_perceive_object.cpp
@@ -23,9 +23,9 @@ int main(int argc, char **argv)
   geometry_msgs::PoseStamped object_pose;
   object_pose.header.stamp = ros::Time::now();
   object_pose.header.frame_id = "/map";
-  object_pose.pose.position.x = 0.25;
-  object_pose.pose.position.y = 0.0;
-  object_pose.pose.position.z = 0.8;
+  object_pose.pose.position.x = 0.5;
+  object_pose.pose.position.y = 0.25;
+  object_pose.pose.position.z = 0.85;
   object_pose.pose.orientation.x = 0.0;
   object_pose.pose.orientation.y = 0.0;
   object_pose.pose.orientation.z = 0.0;
