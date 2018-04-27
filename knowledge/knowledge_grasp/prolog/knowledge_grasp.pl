92a1c91ffa31a8299ba0700080a0f3b80108e0a0
Unknown <maxbahr@uni-bremen.de>
2018-04-27 13:32:57 +0200

    graspsorting implementiert

--- knowledge/knowledge_grasp/prolog/knowledge_grasp.pl
+++ knowledge/knowledge_grasp/prolog/knowledge_grasp.pl
@@ -2,33 +2,62 @@
     [
       find_grasp_pose/4,
       find_grasp_individual/2,
-      sort_grasp_poses/2
+      sort_grasp_poses/3
     ]).
 
-:- use_module(library('storage_place'))
+:- use_module(library(pairs)).
+:- use_module(library('storage_place')).
 :- use_module(library('semweb/rdfs')).
 :- use_module(library('semweb/rdf_db')).
+:- use_module(library('knowrob/rdfs')).
 
 :- rdf_db:rdf_register_ns(knowrob, 'http://knowrob.org/kb/knowrob.owl#',  [keep(true)]).
 :- rdf_db:rdf_register_ns(rdf, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#', [keep(true)]).
 :- rdf_db:rdf_register_ns(xsd, 'http://www.w3.org/2001/XMLSchema#', [keep(true)]).
 
+:- rdf_register_prefix(suturo_storage_place, 'http://knowrob.org/kb/suturo_storage_place.owl#').
 :- rdf_register_prefix(suturo_action, 'http://knowrob.org/kb/suturo_action.owl#').
 :- rdf_register_prefix(suturo_object, 'http://knowrob.org/kb/suturo_object.owl#').
 
 :-  rdf_meta
     find_grasp_pose(r,-,-,-),
     find_grasp_individual(r,-),
-    sort_grasp_poses(r,-).
+    sort_grasp_poses(r,r,-),
+    build_grasp_triple(r,-),
+    build_pose_list(r,-),
+    pair_value(r,-),
+    evaluate_pose_by_height(r,-).
 
-find_grasp_pose(ObjectClassLabel, Translation, Quaternion, Direction):-
-	 find_grasp_individual(ObjectClassLabel, GraspPose),
+find_grasp_pose(ObjectClassLabel, Frame, Translation, Quaternion, Direction):-
+	 sort_grasp_poses(ObjectClassLabel, Frame, SortedGraspList),
+	 member([GraspPose, Translation, Quaternion], SortedGraspList),
 	 rdf(GraspPose, suturo_object:'graspDirection', LitDirection),
-	 strip_literal_type(LitDirection, Direction),
-	 transform_data(GraspPose, (Translation, Quaternion)).
+	 strip_literal_type(LitDirection, Direction).
+
 
 find_grasp_individual(ObjectClassLabel, GraspIndividual):-
 	 owl_class_properties_value(ObjectClassLabel, suturo_object:'graspableAt', GraspIndividual).
 
-sort_grasp_poses(ObjectClassLabel, SortedGraspList):-
-	 findall(X,find_grasp_individual(ObjectClassLabel,X),SortedGraspList).
+build_grasp_tuple([GraspIndividual, ObjectClassLabel, Frame], [GraspIndividual, ObjectClassLabel, Frame, Translation, Quaternion]):-
+	 transform_data(GraspIndividual, (Translation, Quaternion)).
+
+build_pose_list(ObjectClassLabel, Frame, PoseList):-
+	 findall([X,ObjectClassLabel, Frame],find_grasp_individual(ObjectClassLabel,X),GraspIndividualList),
+	 maplist(build_grasp_tuple, GraspIndividualList, PoseList).
+
+evaluate_pose_by_height([Individual, Class, Frame, Translation, Quaternion], K-[Individual, Translation, Quaternion]):-
+	 tf_transform_pose(Frame, "/map", pose(Translation, Quaternion), pose([_, _, MZ], _)),
+	 storage_place(Class, [_,_,SZ], _, _),
+	 K is MZ - SZ.
+
+pair_value(_-V, V).
+
+sort_grasp_poses(ObjectClassLabel, Frame, SortedGraspList):-
+	 build_pose_list(ObjectClassLabel, Frame, PoseList),
+	 maplist(evaluate_pose_by_height, PoseList, KeyedPoseList),
+	 keysort(KeyedPoseList, SortedKeyedList),
+	 maplist(pair_value, SortedKeyedList, SortedGraspList).
+
+pose_test(R):-
+	 sort_grasp_poses(suturo_object:'PringlesSalt', "/ja_milch", List),
+	 member(R, List).
--- knowledge/knowledge_grasp/src/knowledge_grasp_service.cpp
+++ knowledge/knowledge_grasp/src/knowledge_grasp_service.cpp
@@ -15,6 +15,7 @@ std::string createQuery(std::string object_label)
   std::stringstream ss;
     ss << "find_grasp_pose(suturo_object:\'" 
        << object_label << "\',"
+       << "\"" << res.grasp_pose_array.header.frame_id << "\","
        << "Position, Quaternion, Direction)";
     
   return ss.str();
@@ -39,8 +40,26 @@ std::string translateToUnderscore(std::string label){
 
 bool find_grasp_pose(knowledge_msgs::GraspIndividual::Request &req, knowledge_msgs::GraspIndividual::Response &res)
 {
+    res.grasp_pose_array.header.frame_id = "/" + req.object_label;
+
+    //FÜR LOKALE TESTS, FÜR DEN ROBOTER AUSKOMMENTIEREN!!!!
+    res.grasp_pose_array.header.frame_id = "/ja_milch";
+
+    res.force = 0.0;
+      if(req.object_label == "JaMilch"){res.force = 30.0;}
+      if(req.object_label == "TomatoSauceOroDiParma"){res.force = 40.0;}
+      if(req.object_label == "SiggBottle"){res.force = 50.0;}
+      if(req.object_label == "EdekaRedBowl"){res.force = 20.0;}
+      if(req.object_label == "CupEcoOrange"){res.force = 15.0;}
+      if(req.object_label == "KoellnMuesliKnusperHonigNuss"){res.force = 35.0;}
+      if(req.object_label == "PringlesPaprika"){res.force = 50.0;}
+      if(req.object_label == "PringlesSalt"){res.force = 50.0;}
+      if(req.object_label == "HelaCurryKetchup"){res.force = 25.0;}
+      if(req.object_label == "KeloggsToppasMini"){res.force = 15.0;}
+
     Prolog pl;
     PrologQueryProxy bdgs = pl.query(createQuery(req.object_label));
+
  
   for(PrologQueryProxy::iterator it=bdgs.begin();
       it != bdgs.end(); it++)
@@ -52,7 +71,7 @@ bool find_grasp_pose(knowledge_msgs::GraspIndividual::Request &req, knowledge_ms
       res.grasp_pose_array.poses.push_back(poseToAdd);
 
       //PrologBindings bdg = *it;
-      res.grasp_pose_array.header.frame_id = "/" + req.object_label;
+      
 	    //res.grasp_pose_array.pose =  PrologUtil::prologBindingToPose(bdg, "Position", "Quaternion");
       //geometry_msgs::Quaternion quat = res.grasp_pose.pose.orientation;
       //tf::Quaternion new_quat;
@@ -68,17 +87,7 @@ bool find_grasp_pose(knowledge_msgs::GraspIndividual::Request &req, knowledge_ms
 
       
 	  }
-    res.force = 0.0;
-      if(req.object_label == "JaMilch"){res.force = 30.0;}
-      if(req.object_label == "TomatoSauceOroDiParma"){res.force = 40.0;}
-      if(req.object_label == "SiggBottle"){res.force = 50.0;}
-      if(req.object_label == "EdekaRedBowl"){res.force = 20.0;}
-      if(req.object_label == "CupEcoOrange"){res.force = 15.0;}
-      if(req.object_label == "KoellnMuesliKnusperHonigNuss"){res.force = 35.0;}
-      if(req.object_label == "PringlesPaprika"){res.force = 50.0;}
-      if(req.object_label == "PringlesSalt"){res.force = 50.0;}
-      if(req.object_label == "HelaCurryKetchup"){res.force = 25.0;}
-      if(req.object_label == "KeloggsToppasMini"){res.force = 15.0;}
+    
       return true;
     ROS_ERROR_STREAM("LABEL \"" << req.object_label << "\" NICHT ERKANNT! BITTE EINGABEN PRUEFEN!");
     return false; 
